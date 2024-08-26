357a85aebaea5d6fc588246304d095170a568f38
markova-anna <markova.anna@phystech.edu>
2024-08-26 08:28:57 +0300

    new enter

--- get_coef.cpp
+++ get_coef.cpp
@@ -3,15 +3,6 @@
 #include "get_coef.h"
 
 
-/*!
- *\brief enum describes the result of checking what is sbmitted for input
- */
-enum Result
-{
-    OK,     ///< user has entered something that can be used to solve
-    WRONG   ///< there is a problem in users enter
-};
-
 
 /*!
  *\brief moves the position of input of the '\n' or EOF symbol and anylizes buffer 
@@ -30,7 +21,7 @@ static void control_of_input_reload_if_error (double *const a);
  *\param[in,out] good_enter means is there other symbols after a number
  *\return result of checking the enter
  */
-static Result check (double *const a, bool good_enter);
+static Result check (double *const a, bool *const good_enter);
 
 void getting_coefficients (DataForSolvingEquations* fulldataforsolving)
 {
@@ -59,36 +50,40 @@ static void control_of_input_reload_if_error (double *const a)
     while (there_is_symbol != true && cnt < 3) 
     {
         printf("enter again\n");
-        check(a, good_enter);
+        check(&a, &good_enter);
     }
 }
 
-void check(double *const a, bool good_enter)
+void check(double *const a, bool *const good_enter)
 {
-    assert(a != nullptr); 
+    assert(a != nullptr);
+    assert(good_enter != nullptr);
 
     scanf("%lg", a);
     if (clean_buffer() == OK)
-        good_enter == true;
+        good_enter = true;
 
 }
 
 
 static Result clean_buffer()
 {
-    int ch = 0;
+    int ch = getchar();
     int symb_cnt = 0;
 
     while (ch != '\n' && ch != EOF)
     {
+        printf("%c\n", ch);
+
         if (isspace(ch) == false)
+        {
             symb_cnt++;
-    
+        }
         ch = getchar();
     }
 
-    if (symb_cnt != 0)
-        return WRONG;
-    else
+    if (symb_cnt == 0)
         return OK;
+    else
+        return WRONG;
 }
